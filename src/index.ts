253ecdfe423a01d785fafaef65e927d5318682cf
shule517 <shule517@gmail.com>
2016-09-22 13:52:31 +0900

    create.jsのサンプルを追加

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,21 @@
 try { require("source-map-support").install(); } catch (e) { /* empty */ }
 
 console.log('hello');
+
+window.onload = () => {
+    var _worldContainer = new createjs.Stage("canvasElement");
+    var stage = new createjs.Stage("canvasElement");
+    let udon = new createjs.Bitmap('./img/udon.png');
+    udon.scaleX = 2;
+    udon.scaleY = 2;
+    stage.addChild(udon);
+
+    stage.enableMouseOver();
+    stage.addEventListener("click", (event:any) => {
+        alert('(x, y) = (${stage.mouseX}, ${stage.mouseY})');
+    });
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", () => {
+        stage.update();
+    });
+};
